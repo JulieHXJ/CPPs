e345f0e51965df191ab964087c6423c5106d1817
junjun <huangxj1993@gmail.com>
2025-08-22 23:29:42 +0200

    cpp6ex00

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,189 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 17:24:40 by junjun            #+#    #+#             */
+/*   Updated: 2025/08/22 23:28:51 by junjun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+#include <iostream>
+#include <iomanip> // std::setprecision, std::fixed
+#include <limits> //for ovrerflow and underflow checks
+#include <cstdlib> // std::atof, std::atoi strtod/strtof
+#include <cctype> // for char - isprint, isdigit
+
+static bool checkDouble(const std::string &input)
+{
+	if (input.empty())
+		return false;
+	size_t i = 0;
+	if (input[i] == '-' || input[i] == '+'){
+		i++;
+		if (i >= input.size()) return false;
+	}
+	bool hasDecimal = false;
+	bool hasDigit = false;
+	for (; i < input.size(); i++)
+	{
+		if (std::isdigit(static_cast<unsigned char>(input[i])))
+			hasDigit = true;
+		else if (input[i] == '.' && !hasDecimal)
+			hasDecimal = true;
+		else
+			return false;
+	}
+	return hasDigit && hasDecimal;
+}
+
+static bool checkInt(const std::string& input) {
+    if (input.empty())
+		return false;
+	size_t i = 0;
+	if (input[i] == '-' || input[i] == '+'){
+		i++;
+		if (i >= input.size())
+			return false; // no digits after sign
+	}
+	for (; i < input.size(); ++i)
+	{
+		if (!std::isdigit(static_cast<unsigned char>(input[i])))
+			return false;
+	}
+	return true;
+}
+		
+//convert char/int/float to double
+static bool toDouble(const std::string &input, double &d)
+{
+	// char
+	if (input.size() == 1 && !std::isdigit(static_cast<unsigned char>(input[0])))
+	{
+		d = static_cast<double>(input[0]);
+		return true;
+	}
+	
+	//float
+	if (input.size() >= 2 && input[input.size() - 1] == 'f')
+	{
+		std::string floatInput = input.substr(0, input.size() - 1);
+		if (!checkDouble(floatInput))
+			return false; // invalid float input
+		char *endptr; // check conversion finished or not
+		d = std::strtod(floatInput.c_str(), &endptr);
+		return endptr && *endptr == '\0';
+	}
+
+	//double
+	if (checkDouble(input))
+	{
+		char *endptr;
+		d = std::strtod(input.c_str(), &endptr);
+		return endptr && *endptr == '\0';
+	}
+
+	//int
+	if (checkInt(input))
+	{
+		char *endptr;
+		long l = std::strtol(input.c_str(), &endptr, 10);
+		if (endptr && *endptr == '\0')
+		{
+			d = static_cast<double>(l);
+			return true;
+		}
+	}
+	return false;
+}
+
+// print functions
+static void printChar(double c)
+{
+	int i = static_cast<int>(c);
+	//check nan or inf	
+	if (c != c // NaN
+		|| c > 127.0 || c < 0.0)
+	{
+		std::cout << "char: impossible\n";
+		return;
+	}
+	if (!isprint(static_cast<unsigned char>(i)))
+		std::cout << "char: Non displayable\n";
+	else
+		std::cout << "char: '" << static_cast<char>(i) << "'\n";
+		
+}
+
+static void printInt(double i)
+{
+	if (i != i // NaN
+		|| i > std::numeric_limits<int>::max() || i < std::numeric_limits<int>::min())
+		std::cout << "int: impossible\n";
+	else
+		std::cout << "int: " << static_cast<int>(i) << "\n";
+}
+
+static void printDouble(double d)
+{
+	std::cout << "double: " << std::fixed << std::setprecision(1) << d << "\n";
+}
+
+static void printFloat(double f)
+{
+	if (f > static_cast<double>(std::numeric_limits<float>::max()) 
+		|| f < static_cast<double>(-std::numeric_limits<float>::max()))
+	{
+		std::cout << "float: impossible\n";
+		return;
+	}
+	float fval = static_cast<float>(f);
+	std::cout << "float: " << std::fixed << std::setprecision(1) << fval << "f\n";
+}
+
+void ScalarConverter::convert(const std::string &input)
+{
+	// output message directly if nan inf or inff 
+	if (input == "nan" || input == "+inf" || input == "-inf")
+	{
+		std::cout << "char: impossible\n";
+		std::cout << "int: impossible\n";
+		std::cout << "float: " << input << "f\n";
+		std::cout << "double: " << input << "\n";
+		return;
+	}
+	if (input == "nanf" || input == "+inff" || input == "-inff")
+	{
+		std::cout << "char: impossible\n";
+		std::cout << "int: impossible\n";
+		std::cout << "float: " << input << "\n";
+		std::cout << "double: " << input.substr(0, input.length() - 1) << "\n"; // remove 'f'
+		return;
+	}
+
+	// convert input to double
+	double d;
+	if (!toDouble(input, d))
+	{
+		std::cout << "char: impossible\n";
+		std::cout << "int: impossible\n";
+		std::cout << "float: impossible\n";
+		std::cout << "double: impossible\n";
+	}
+	//print output message
+	printChar(d);
+	printInt(d);
+	printFloat(d);
+	printDouble(d);	
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2) {
+		ScalarConverter::convert(argv[1]);
+	}
+	return 0;
+}
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 17:15:43 by junjun            #+#    #+#             */
+/*   Updated: 2025/08/21 23:14:38 by junjun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+
+class ScalarConverter
+{
+private:
+	// ScalarConverter() = delete; for c++11
+	ScalarConverter();// private constructor to prevent instantiation
+	ScalarConverter(const ScalarConverter&); 
+	ScalarConverter& operator=(const ScalarConverter&);
+	
+public:
+	static void convert(const std::string &input);
+};
+
+
+#endif // SCALARCONVERTER_HPP
