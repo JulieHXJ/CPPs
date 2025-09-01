3a7c5c6ddde722f12171cb65074f0ebe75de4281
junjun <huangxj1993@gmail.com>
2025-09-01 12:44:23 +0200

    cpp7

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 14:51:44 by junjun            #+#    #+#             */
-/*   Updated: 2025/08/28 16:52:56 by xhuang           ###   ########.fr       */
+/*   Updated: 2025/09/01 12:43:06 by junjun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,56 @@
 #include "Data.hpp"
 #include <iostream>
 
+// int main()
+// {
+// 	Data data;
+// 	data.id = 42;
+// 	data.name = "Example";
+// 	data.value = 3.14;
+// 	// Data* ptr = &data; //for checking address
+	
+// 	std::cout << "Original Data - id: " << data.id << ", name: " << data.name << ", value: " << data.value << "\n";
+// 	std::cout << "Original Data address: " << &data << "\n";
+
+// 	//serialize
+// 	uintptr_t raw = Serializer::serialize(&data);
+// 	std::cout << "raw uintptr_t: " << raw << "\n";
+
+// 	//deserialize
+// 	Data* newData = Serializer::deserialize(raw);
+// 	std::cout << "Deserialized Data - id: " << newData->id << ", name: " << newData->name << ", value: " << newData->value << "\n";
+// 	std::cout << "Deserialized Data address: " << newData << "\n";//should be diff
+
+
+// 	//modify newData to see if it affects original data
+// 	newData->id = 100;
+//     std::cout << "data.id after change newData: " << data.id << std::endl;
+
+// 	return 0;
+	
+// }
+
+
+
 int main()
 {
-	Data data;
-	data.id = 42;
-	data.name = "Example";
-	data.value = 3.14;
-	// Data* ptr = &data; //for checking address
-	
-	std::cout << "Original Data - id: " << data.id << ", name: " << data.name << ", value: " << data.value << "\n";
-	std::cout << "Original Data address: " << &data << "\n";
-
-	//serialize
-	uintptr_t raw = Serializer::serialize(&data);
-	std::cout << "raw uintptr_t: " << raw << "\n";
-
-	//deserialize
-	Data* newData = Serializer::deserialize(raw);
-	std::cout << "Deserialized Data - id: " << newData->id << ", name: " << newData->name << ", value: " << newData->value << "\n";
-	std::cout << "Deserialized Data address: " << newData << "\n";//should be diff
-
-
-	//modify newData to see if it affects original data
-	newData->id = 100;
-    std::cout << "data.id after change newData: " << data.id << std::endl;
-
-	return 0;
-	
-}
-
-
-/**
- * int main()
-{
-    Data original(5, "Katzen", 'k');
+    Data original;
+	original.id = 5;
+	original.name = "Katzen";
+	original.value = 9.99;
 
     uintptr_t res = Serializer::serialize(&original);
     Data *ultRes = Serializer::deserialize(res);
 
-    std::cout << PURPLE << "Original pointer address: " << &original << std::endl;
-    original.printVars();
-    std::cout << BLUE << "Result pointer address: " << &ultRes << std::endl;
-    ultRes->printVars();
+    std::cout << "Original pointer address: " << &original << std::endl;
+    // original.printVars();
+    std::cout << "Result pointer address: " << &ultRes << std::endl;
+    // ultRes->printVars();
 
     if (&original == ultRes)
-        std::cout << GREEN << "SUCCESS - Pointers are equal!" << std::endl;
+        std::cout << "SUCCESS - Pointers are equal!" << std::endl;
     else
-        std::cout << RED << "FAILURE - Pointers are not equal!" << std::endl;
+        std::cout << "FAILURE - Pointers are not equal!" << std::endl;
 
     return 0;
 }
- */
--- cpp07/ex02/Array.tpp
+++ cpp07/ex02/Array.tpp
@@ -3,79 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   Array.tpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:05:36 by xhuang            #+#    #+#             */
-/*   Updated: 2025/08/28 17:09:50 by xhuang           ###   ########.fr       */
+/*   Updated: 2025/09/01 12:37:44 by junjun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef ARRAY_TPP
 #define ARRAY_TPP
-#include "Array.hpp"
 
 template <typename T>
-Array<T>::Array() : array_(NULL), size_(0) {}
+Array<T>::Array() : _size(0), _data(NULL) {}
 
 template <typename T>
-Array<T>::Array(unsigned int n) : array_(new T[n]()), size_(n) {}
+Array<T>::Array(unsigned int n) 
+: _size(n), _data(n ? new T[n]() : NULL) {}
 
 template <typename T>
-Array<T>::Array(const Array& other) : _array(NULL), _size(other._size)
-{
-	if (_size > 0)
-	{
-		_array = new T[_size];
-		for (unsigned int i = 0; i < _size; i++)
-			_array[i] = other._array[i];
+Array<T>::Array(const Array& other) 
+:  _size(other._size), _data(other._size ? new T[other._size]() : NULL) {
+	for (size_t i = 0; i < _size; ++i) {
+		_data[i] = other._data[i];
 	}
 }
 
 template <typename T>
-Array<T>::~Array()
-{
-	delete[] _array;
-	std::cout << "destructor called" << std::endl;
-}
+Array<T>::~Array() { delete[] _data;}
 
 template <typename T>
 Array<T>& Array<T>::operator=(const Array& other)
 {
 	if (this != &other)
 	{
-		delete[] _array;
-		_size = other._size;
-		if (_size > 0)
-		{
-			_array = new T[_size];
-			for (unsigned int i = 0; i < _size; i++)
-				_array[i] = other._array[i];
+		T* newData = other._size ? new T[other._size]() : NULL;
+		for (size_t i = 0; i < other._size; ++i) {
+			newData[i] = other._data[i];
 		}
-		else
-			_array = NULL;
+		delete[] _data;
+		_data = newData;
+		_size = other._size;
 	}
 	return *this;
 }
 
 template <typename T>
-T& Array<T>::operator[](unsigned int index)
+T& Array<T>::operator[](size_t index)
 {
-	if (index >= _size)
-		throw std::out_of_range("Index out of bounds");
-	return _array[index];
+	if (index >= _size) {
+		throw std::out_of_range("Array index out of bounds");
+	}
+	return _data[index];
 }
 
 template <typename T>
-const T& Array<T>::operator[](unsigned int index) const
+const T& Array<T>::operator[](size_t index) const
 {
 	if (index >= _size)
-		throw std::out_of_range("Index out of bounds");
-	return _array[index];
+		throw std::out_of_range("Array index out of range");
+	return _data[index];
 }
 
 template <typename T>
-unsigned int Array<T>::size() const
+size_t Array<T>::size() const
 {
 	return _size;
 }
